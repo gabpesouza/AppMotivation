7b8ba1572f8d094403face24a28bf176d30477d8
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2023-02-08 14:49:02 -0300

    Projeto App motivation criado

---
+++ app/src/main/java/br/com/gabriel/appmotivation/MainActivity.kt
@@ -0,0 +1,25 @@
+package br.com.gabriel.appmotivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import br.com.gabriel.appmotivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(),View.OnClickListener() {
+
+    private lateinit var binding:ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onClick(view: View) {
+        
+    }
+}
